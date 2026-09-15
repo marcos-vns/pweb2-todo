2e6c4141fce9a94e92953b01848855482cdf9c2d
Marcos Vinicius <themarcosvn@gmail.com>
2026-09-15 19:44:00 -0300

    Alterações em sala

--- src/handleTasks.js
+++ src/handleTasks.js
@@ -1,8 +1,40 @@
 export function createTask(taskText, tasksListElement){
     const task = document.createElement('div');
+    task.classList.add('task');
+    task.classList.add('undone-task');
+
+    const checkbox = document.createElement('input');
+    checkbox.type = 'checkbox';
+
+    task.appendChild(checkbox);
+
     const taskTitle = document.createElement('span');
     taskTitle.innerText = taskText;
     task.appendChild(taskTitle);
+
+    const taskInfos = document.createElement('');
+
+    const currentDate = getCurrentDate();
+
+    const createdAt = document.createElement('p');
+    createdAt.innerText = `Criada em: ${currentDate}`;
+    task.appendChild(createdAt);
     
     tasksListElement.appendChild(task);
+}
+
+function getCurrentDate(){
+
+    const now = new Date();
+    const day = now.getDay();
+    const month = now.getMonth();
+    const year = now.getFullYear();
+
+    const currentDate = `${day}/${month}/${year}`
+
+    return currentDate;
+}
+
+export function updateProgress(){
+
 }
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
-import { createTask } from "./handleTasks";
+import { createTask, updateProgress } from "./handleTasks";
 
 const username = "Marcos Vinicius";
+let tasks = 0;
 
 const app = document.getElementById('app');
 
@@ -26,10 +27,25 @@ buttonElement.type = "submit";
 buttonElement.innerText = "Adicionar";
 app.appendChild(buttonElement);
 
+const taskListProgressElement = document.createElement('p');
+
+const noTaskElement = document.createElement('div');
+const noTaskIcon = document.createElement('img');
+const noTaskMessageElement = document.createElement('p');
+noTaskMessageElement.innerText = "Nenhum registro encontrado!";
+noTaskElement.appendChild(noTaskIcon);
+noTaskElement.appendChild(noTaskMessageElement);
+
+// noTaskElement.addEventListener('toggle', (event) => );
+
+app.appendChild(noTaskElement);
+
 const tasksListElement = document.createElement('div');
 app.appendChild(tasksListElement);
 
 buttonElement.addEventListener('click', () => {
+  if(inputElement.value === "") return
+
   createTask(inputElement.value, tasksListElement);
   inputElement.value = "";
 });
